63f2a508f2dbd2dc5c92ed6d22cc12c39e878637
김민국 <mgkim@woowahan.com>
2022-01-08 21:53:52 +0900

    github page 초기설정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          민국 ♡ 현주
         </p>
         <a
           className="App-link"
