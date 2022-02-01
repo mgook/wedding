626a906e45ce3b8765c5077a5be6c6bb6fa1e4ad
김민국 <mgkim@woowahan.com>
2022-02-01 23:01:40 +0900

    og 설정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { MetaTags } from 'react-meta-tags';
 import './App.css';
 import { Calendar } from './calendar/Calendar';
 import LivereComment from './comment/LivereComment';
@@ -9,10 +10,15 @@ import Location from './location/Location';
 import Navbar from './navbar/Navbar';
 import Top from './top/Top';
 
-
 function App() {
   return (
     <div className="App">
+      <MetaTags>
+        <title>민국 ♡ 현주</title>
+        <meta property="og:title" content="민국♡현주, 결혼합니다" />
+        <meta name="og:description" content="일시: 2022.03. 13 (일) 오후 2시\n장소: 노블발렌티 대치" />
+        <meta property="og:image" content="https://github.com/mgook/wedding/blob/master/src/assets/images/og_image.jpg" />
+      </MetaTags>
       <Top />
       <Navbar />
       <Intro />
