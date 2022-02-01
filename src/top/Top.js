d12ddd401558ee78879dd1153ba3a220b2323bb4
김민국 <mgkim@woowahan.com>
2022-02-01 13:54:51 +0900

    가로세로 디버깅

--- src/top/Top.js
+++ src/top/Top.js
@@ -1,7 +1,12 @@
+import { useState, useEffect } from 'react';
 import './Top.css';
 
 
 function Top() {
+
+    const { height, width } = useWindowDimensions();
+    const initHeight = { height };
+
     return (
         <div className="Top">
             <div className='Top-invitation-box'>
@@ -9,7 +14,7 @@ function Top() {
             </div>
 
             <div className='Top-text-box'>
-                <div className="Top-title">22. 03. 13.</div>
+                <div className="Top-title">{/* 22. 03. 13. */} width: {width} ~ height: {height}</div>
                 <div className="Top-sub-title">김민국 - 이현주</div>
                 <div className="Top-desc">
                     2020월 03월 13일 (일) <b>오후 2시</b><br />
@@ -22,5 +27,28 @@ function Top() {
     );
 }
 
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
+}
+
+function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
+}
 
 export default Top;
